167e8cd3f76906f6ffb7300828916513de6f41a8
nika7724 <nika0147@stud.kea.dk>
2022-12-06 23:48:31 +0100

    update inventory change updatebutton inventory.prodcutQuantity PUT inventory.id

--- Static/js/inventory.js
+++ Static/js/inventory.js
@@ -1,40 +1,33 @@
 //Rest API to connect to back-end
 const inventoryApi ='http://localhost:8080/inventory';
+const itemDescriptionsApi='http://localhost:8080/itemDescriptions';
 
-//Table itself retrieved from the HTML document
 const inventoryTable = document.getElementById('inventory-table-body')
-
-const filteringType = document.getElementById('type-drop-down')
+const dropDownInventory = document.getElementById('type-drop-down')
 
 //Empty array to be filled with the Jason inventory objects
 let inventoryArray;
 
-//creates a table taking data from inventoryArray
+//Create table
 async function createTable(inventory) {
-    if (filteringType.value === "All") {
-        createRow(inventory)
-    }
-    else if(inventory.itemDescription === filteringType.value)
-    {
-        createRow(inventory)
-    }
+          createRow(inventory)
 }
 
-//create table(row by row) using data from items
+//Create table(row by row)
 function createRow(inventory) {
-    let rowCount = inventoryTable.rows.alength;
+    let rowCount = inventoryTable.rows.length;
     let row = inventoryTable.insertRow(rowCount)
     let cellCount = 0
 
-//inserting each cell of the 1st Column(Name)
+//1st Column(Name)
     let cell = row.insertCell(cellCount++)
     cell.innerHTML = inventory.productName
 
-//inserting each cell of the 2nd Column(Type)
+//2nd Column(Type)
     cell = row.insertCell(cellCount++)
     cell.innerHTML = inventory.itemDescription.description
 
-//inserting each cell of 3rd column(Quantity)
+//3rd column(Quantity)
     cell = row.insertCell(cellCount++)
     let inputFieldUpdate = document.createElement('input')
     inputFieldUpdate.type = "number"
@@ -42,7 +35,7 @@ function createRow(inventory) {
     inputFieldUpdate.className = "inventoryTable"
     cell.appendChild(inputFieldUpdate)
 
-    //inserting each cell of 4th column(To Order)
+    //4th column(To Order)
     cell = row.insertCell(cellCount++)
     /*let inputFieldOrderQuantity = document.createElement("input");
     inputFieldOrderQuantity.type = "number"
@@ -50,63 +43,41 @@ function createRow(inventory) {
     inputFieldOrderQuantity.className = "inventoryTable"
     cell.appendChild(inputFieldOrderQuantity)*/
 
-    //inserting each cell of 5th column a save button to update the quantity
-    cell = row.insertCell(cellCount)
+    //5th column SaveButton
+    cell = row.insertCell(cellCount++)
     let updateButton = document.createElement('button')
     updateButton.innerText = "Save"
     updateButton.className = "inventoryTable"
     cell.appendChild(updateButton);
 
-    // update button from 5th column to update quantity
+    //5th column Update quantity: SaveButton
     updateButton.addEventListener('click', () => {
-        if(inputFieldUpdate.value < inventory.itemDescription.description) {
+        if(inputFieldUpdate.value < inventory.productQuantity) {
             updateInventory(inventory, inputFieldUpdate.value);
         }
     })
-//delete button
+
+//5th column DeleteButton
     let deleteButton = document.createElement('button')
     deleteButton.innerText = "Delete"
     deleteButton.className = "inventoryTable"
     cell.appendChild(deleteButton);
 
+    //5th column Delete the raw DeleteButton
     deleteButton.addEventListener("click", function (evt) {
         deleteRequest(evt, inventory['id'], inventory).then(r => console.log(r));
     });
-
 }
 
-function showByType() {
-    var value = filteringType.options[filteringType.selectedIndex].value;
-    console.log(value);
-    let typeValue = "?itemDescription.description" + value;
-    $.ajax({
-        type: "GET",
-        contentType: "application/json",
-        url: inventoryApi + typeValue,
-        dataType: 'json',
-        cache: false,
-        timeout: 600000,
-        success: function (data) {
-            console.log(data);
-            // clear();
-        },
-        error: function (e) {
-            console.log(e);
-        }
-    });
-}
-
-
-
-//update the quantity.
+//Update the quantity
 async function updateInventory(inventory, newQuantity) {
     inventory.productQuantity = newQuantity;
     await restUpdateInventory(inventory);
 }
 
-//sends the PUT request with the new info attached in the body
+//PUT
 async function restUpdateInventory(inventory) {
-    const url = inventoryApi + "/" + inventory.itemDescription.id;
+    const url = inventoryApi + "/" + inventory.id;
     const fetchOptions = {
         method: 'PUT',
         headers: {
@@ -120,6 +91,7 @@ async function restUpdateInventory(inventory) {
     //return jsonString;
 }
 
+//DELETE
 async function deleteRequest(evt, id) {
     const fetchOptions = {
         method: "DELETE",
@@ -137,23 +109,62 @@ async function deleteRequest(evt, id) {
     return response;
 }
 
+/*const getPost = async  () =>{
+    const response = await fetch(itemDescriptionsApi);
+    console.log(response)
+    const data = response.json();
+    return data;
+};*/
+//Drop-down menu
+const displayOption = async () => {
+    const options =await fetchInventory(itemDescriptionsApi);
+    for (option of options) {
 
+        const newOption = document.createElement("option");
+        console.log(option);
+        newOption.id = option.description;
+        newOption.value = option.description;
+        newOption.text = option.description;
+dropDownInventory.appendChild(newOption);
+    }
+};
+displayOption();
 
-//===== gets the json array objects =====
+/*function showByType() {
+    var value = filteringType.options[filteringType.selectedIndex].value;
+    console.log(value);
+    let typeValue = "?itemDescription.description=" + value;
+    $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: inventoryApi + typeValue,
+        dataType: 'json',
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            console.log(data);
+            // clear();
+        },
+        error: function (e) {
+            console.log(e);
+        }
+    });
+}*/
+
+//Get Json array objects
 async function fetchInventory(url) {
     return fetch(url).then(response => response.json())
 }
 
-//=====populates an array of inventory and creates a table=====
+//Populates an array of inventory and creates a table
 async function doFetchInventory() {
     //clear()
     inventoryArray = await fetchInventory(inventoryApi)
     inventoryArray.forEach(createTable)
+    console.log(inventoryArray)
+
 
     document.getElementById("create-button").onclick =function () {
         location.href='create-inventory.html';
     }
-
-    filteringType.addEventListener('change', doFetchInventory)
-
 }
